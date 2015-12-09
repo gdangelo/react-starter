c4b390f2939b867c3504cc6412377da7fa2e9a29
Grégory D'Angelo <dangelo.gregory@gmail.com>
2015-12-09 16:25:31 +0100

    Server code source using WebpackDevServer

---
+++ tools/serve.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('./webpack.config');
+
+/**
+ * Launches Webpack dev server.
+ */
+module.exports = function server(){
+  return new Promise(function(resolve, reject){
+    new WebpackDevServer(webpack(webpackConfig), {
+      publicPath: webpackConfig.output.publicPath
+    }).listen(3000, 'localhost', function(err, result){
+      if (err){
+        console.log(err);
+        reject(err);
+      }
+
+      console.log("Listening at localhost:3000");
+      resolve();
+    });
+  });
+};
--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,10 +1,15 @@
 var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/app.jsx',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/app.jsx'
+  ],
   output: {
     path: path.resolve(__dirname, '../build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/build/'
   },
   module: {
     loaders: [
